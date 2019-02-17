bb7dd1395319317136b99b842ef68a7406ea8de1
Sphinx5 <reptile295@gmail.com>
2019-02-17 18:14:52 +0100

    Début du code pour le projet

---
+++ src/um3/projettransversal/Client.java
@@ -0,0 +1,68 @@
+package um3.projettransversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+
+public class Client {
+	
+	public static void main(String[] zero) {
+		
+		
+		Socket socket;
+		BufferedReader in;
+		PrintWriter out;
+
+		try {
+//Connexion serveur
+			socket = new Socket(InetAddress.getLocalHost(), 2019);	
+		    System.out.println("Demande de connexion");
+		    
+//Message de bienvenue du serveur
+		    in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+		    String message_distant = in.readLine();
+		    System.out.println(message_distant);
+//Demende si le joueur est prêt
+		    String message_distant1 = in.readLine();
+		    System.out.println(message_distant1);
+//Saisie du joueur s'il est prêt ou non et envoie au serveur
+		    Scanner sc = new Scanner(System.in);
+		    String message = sc.next();
+		    out = new PrintWriter(socket.getOutputStream());
+		    out.println(message);
+		    out.flush();
+		    
+//Message retour suite à la réponse du joueur
+		    String message_distant2 = in.readLine();
+		    System.out.println(message_distant2);
+		    
+		   /* String message2 = sc.next();
+		    out = new PrintWriter(socket.getOutputStream());
+		    out.println("Client : " + message2);
+		    out.flush();
+		    
+		    String message_distant2 = in.readLine();
+		    System.out.println("Serveur : " + message_distant2);*/
+		    
+		    
+		    sc.close(); 
+		    socket.close();
+		       
+		}
+		catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/um3/projettransversal/Serveur.java
@@ -0,0 +1,65 @@
+package um3.projettransversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur {
+	
+	public static void main(String[] zero) {
+		
+		ServerSocket socketserver  ;
+		Socket socketduserveur ;
+		BufferedReader in;
+		PrintWriter out;
+		
+		try {
+		
+			socketserver = new ServerSocket(2019);
+			System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());
+			socketduserveur = socketserver.accept(); 
+		    System.out.println("Un joueur s'est connecté");//si un joueur est connecté
+//Accueil du joueur		    
+			out = new PrintWriter(socketduserveur.getOutputStream());
+		    out.println("Bienvenue joueur !\nVous allez débuter une partie de Mastermind. Prêt(e) ? (oui/non)");
+		    out.flush();
+//Récupère la réponse du joueur s'il veux jouer		    
+		    in = new BufferedReader (new InputStreamReader (socketduserveur.getInputStream()));
+		    String message = in.readLine();
+		    System.out.println("Réponse joueur : " + message);
+
+//lancement de la partie ou pas
+			if(message != "non") {
+//déroulement de la partie
+				String mastermind = "Rouge-Bleu-Vert-Jaune";
+				out.println("Début de la partie ! Il y a 4 couleurs entre le Bleu, le Jaune, le Vert et le Rouge.\n"
+						+ "Vous devrez trouvez la bonne combinaison pour gagner. Petite précision les couleurs devront avoir leur initiale en majuscule et les coleurs séparés par un tiret.");
+				out.flush();
+			}
+			else {
+				out.println("Une autre fois alors !");
+				out.flush();
+			}
+			
+			
+		   /* String message1 = in.readLine();
+		    System.out.println(message1);
+		    out.println(message1);
+			out.flush();*/
+
+		                
+		    socketduserveur.close();
+		    socketserver.close();
+		    
+		        
+		}
+		catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
