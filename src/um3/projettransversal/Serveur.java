91d86bc36c5a4e3f4a2f3f1a3615606d3fd3a731
Sphinx5 <reptile295@gmail.com>
2019-03-01 16:32:54 +0100

    SuiteCouleur, y a en 8.

--- src/um3/projettransversal/Serveur.java
+++ src/um3/projettransversal/Serveur.java
@@ -6,17 +6,22 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Serveur {
+public class Serveur
+{
 	
-	public static void main(String[] zero) {
+	public static void main(String[] zero)
+	{
 		
 		ServerSocket socketserver  ;
 		Socket socketduserveur ;
 		BufferedReader in;
 		PrintWriter out;
 		
-		try {
+		try
+		{
 		
 			socketserver = new ServerSocket(2019);
 			System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());
@@ -26,22 +31,35 @@ public class Serveur {
 			out = new PrintWriter(socketduserveur.getOutputStream());
 		    out.println("Bienvenue joueur !\nVous allez débuter une partie de Mastermind. Prêt(e) ? (oui/non)");
 		    out.flush();
-		    
 //Récupère la réponse du joueur s'il veux jouer		    
 		    in = new BufferedReader (new InputStreamReader (socketduserveur.getInputStream()));
 		    String message = in.readLine();
 		    System.out.println("Réponse joueur : " + message);
 
 //Lancement de la partie ou pas
-			if(message != "non") {
+			if(message != "non")
+			{
+				
 //Déroulement de la partie
-				String code = "Rouge-Bleu-Vert-Jaune";
-				out.println("Début de la partie ! Il y a 4 couleurs entre le Bleu, le Jaune, le Vert et le Rouge.\nVous devrez trouvez la bonne combinaison pour gagner. Petite précision les couleurs devront avoir leur initiale en majuscule et les coleurs séparés par un tiret.");
+				List<SuiteCouleur> combinaison = new ArrayList<SuiteCouleur>();
+				SuiteCouleur[] allColors = SuiteCouleur.values();
+				System.out.println(allColors);
+				
+				out.println("Début de la partie ! Il y a 6 couleurs entre le Bleu, le Jaune, le Vert, le Rouge, le Blanc et le Noir."
+						+ "\nVous devrez trouvez la bonne combinaison pour gagner. Petite précision les couleurs devront avoir leur initial "
+						+ "en majuscule et les coleurs séparés par un tiret.");
 				out.flush();
 				String message1 = in.readLine();
 			    System.out.println("Réponse joueur : " + message1);
 			    
-			    if(message1 == code) {
+			    List<Serveur> tentative = new ArrayList<Serveur>();
+//			    tentative.add(message1.slip("-"));
+			    for(int i = 0; i <= 6; i++)
+			    {
+			    	System.out.println(i);
+			    }
+			    
+			   /* if(message1 == ) {
 			    	out.println("Juste");
 					out.flush();
 			    }
@@ -53,17 +71,24 @@ public class Serveur {
 			else {
 				out.println("Une autre fois alors !");
 				out.flush();
-			}
+			}*/
 			
-			            
+			
+			
 		    socketduserveur.close();
 		    socketserver.close();
-		    
-		        
+		    }
 		}
-		catch (IOException e) {
-			
+		catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 	}
+
+	@Override
+	public String toString() {
+		return "Couleur : ";
+	}
+	
+	
 }
---
+++ src/um3/projettransversal/SuiteCouleur.java
@@ -0,0 +1,12 @@
+package um3.projettransversal;
+
+public enum SuiteCouleur {
+	Rouge,
+	Bleu,
+	Vert, 
+	Jaune, 
+	Blanc,
+	Gris,
+	Rose,
+	Noir;
+}
