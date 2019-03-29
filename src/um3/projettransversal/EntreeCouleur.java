ab4147a83bc329d939fc1930f82e93e0ad8c087b
natso <natso@192.168.207.94>
2019-03-29 17:12:20 +0100

    EntreeCouleur (Nathan) Seconde version

---
+++ src/um3/projettransversal/EntreeCouleur.java
@@ -0,0 +1,15 @@
+package um3.projettransversal;
+
+import java.util.ArrayList;
+public class EntreeCouleur {
+    public static ArrayList<String> EntreeCouleur(String Couleur) {
+        String[] partie = Couleur.split("-");
+        ArrayList<String> tab = new ArrayList<String>();
+        for(int i=0;i<partie.length; i++) {
+            tab.add(partie[i]);
+        }
+        return tab;
+    }
+    
+
+}
